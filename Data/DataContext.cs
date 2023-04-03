003e689fbb064c08ffced8814a2ebfca0432f3f0
Stephen Cunningham <stevecunningham@outlook.com>
2023-04-03 06:50:42 -0500

    added ef and db

---
+++ Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }    
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+
+
+        static readonly string connectionString = "Server=localhoset; User ID=root; Password=password; Database=superhero";
+        public static MySqlConnection GetConnection()
+        {
+            MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
+            conn_string.Server = "127.0.0.1";
+            conn_string.UserID = "root";
+            conn_string.Password = "password";
+            conn_string.Database = "SuperHeroData";
+
+
+            return new MySqlConnection(conn_string.ToString());
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using SuperHeroAPI.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -15,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("SuperHeroOrigins");
 
 app.UseHttpsRedirection();
 

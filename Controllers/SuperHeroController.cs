07114e0e858620248fb7475cc404f8038dadd71b
Stephen Cunningham <stevecunningham@outlook.com>
2023-04-06 21:41:13 -0500

    api is done

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -22,5 +22,46 @@ namespace SuperHeroAPI.Controllers
             //to get this to work had to remove System.Data.Entity since .NET will default to it
             return Ok(await _dataContext.SuperHeroes.ToListAsync());
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHeroes(SuperHero hero)
+        {
+            hero.Id = 0; //circumvent identity error from user
+            _dataContext.SuperHeroes.Add(hero);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var existingHero = await _dataContext.SuperHeroes.FindAsync(hero.Id);
+            if (existingHero == null) 
+                return BadRequest("Hero not found"); 
+            
+
+            existingHero.Name = hero.Name;
+            existingHero.FirstName = hero.FirstName;    
+            existingHero.LastName = hero.LastName;  
+            existingHero.Place = hero.Place;  
+            
+            await _dataContext.SaveChangesAsync();
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());   
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var existingHero = await _dataContext.SuperHeroes.FindAsync(id);
+            if (existingHero == null)
+                return BadRequest("Hero not found");
+
+            _dataContext.SuperHeroes.Remove(existingHero);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
+        }
+        
     }
 }
